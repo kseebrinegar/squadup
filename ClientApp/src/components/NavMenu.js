5d82e705ef30c062416781305fb3c4f263df02e3
MattMcLennan <mclennan6891@gmail.com>
2018-07-19 21:40:52 -0400

    changes all occurunces of wrong project name and references everywhere

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -8,7 +8,7 @@ export default props => (
   <Navbar inverse fixedTop fluid collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to={'/'}>squaduptemplate</Link>
+        <Link to={'/'}>squadup</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace squaduptemplate
+namespace squadup
 {
     public class Program
     {
