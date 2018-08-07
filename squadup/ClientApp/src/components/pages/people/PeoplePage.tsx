1845b1293651e55f23028343608123c51c606c37
kseebrinegar <ksee.brinegar@hotmail.com>
2018-08-06 23:44:27 -0700

    changed prettier.tabWid to 4

--- squadup/ClientApp/src/components/pages/people/PeoplePage.tsx
+++ squadup/ClientApp/src/components/pages/people/PeoplePage.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
 class PeoplePage extends React.Component {
-  public render() {
-    return (
-      <React.Fragment>
-        <div>People page</div>
-      </React.Fragment>
-    );
-  }
+    public render() {
+        return (
+            <React.Fragment>
+                <div>People page</div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default PeoplePage;
